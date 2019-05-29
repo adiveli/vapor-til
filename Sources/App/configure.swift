9e6a050a0b9f564072a4ab67630371084c91482f
Adi <adi.veli@ymail.com>
2019-05-29 20:51:53 +0300

    Some test

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -16,31 +16,35 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
-
+    
+    
+    // Configure a database
     var databases = DatabasesConfig()
-    
-    // Configure a SQLite database
-    
+    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+    let username = Environment.get("DATABASE_USER") ?? "vapor"
     
     let databaseName: String
     let databasePort: Int
-    
-    if(env == .testing){
+    if (env == .testing) {
         databaseName = "vapor-test"
-        databasePort = 5433
-    } else {
-        databaseName = "vapor"
+        if let testPort = Environment.get("DATABASE_PORT") {
+            databasePort = Int(testPort) ?? 5433
+        } else {
+            databasePort = 5433
+        }
+    }
+    else {
+        databaseName = Environment.get("DATABASE_DB") ?? "vapor"
         databasePort = 5432
     }
-    
-    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
-    let username = Environment.get("DATABASE_USER") ?? "vapor"
     let password = Environment.get("DATABASE_PASSWORD") ?? "password"
-    
-    let databaseConfig = PostgreSQLDatabaseConfig(hostname: hostname, port: databasePort, username: username, database: databaseName, password: password)
-
-
-    var database = PostgreSQLDatabase(config: databaseConfig)
+    let databaseConfig = PostgreSQLDatabaseConfig(
+        hostname: hostname,
+        port: databasePort,
+        username: username,
+        database: databaseName,
+        password: password)
+    let database = PostgreSQLDatabase(config: databaseConfig)
     databases.add(database: database, as: .psql)
     services.register(databases)
 
